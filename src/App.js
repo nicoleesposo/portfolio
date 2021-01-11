43a20e56f51a23c7123a10e3283ad826e12babb1
unknown <nicoleesposo.web@gmail.com>
2021-01-11 15:42:16 +0800

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Home from '../src/Home'
-import { HashRouter, Route, } from "react-router-dom";
+// import { HashRouter, Route, } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <Route exact path = '/' component = { Home } />
-    </HashRouter>
+    <>
+      <Home />
+    </>
+    // <HashRouter basename={process.env.PUBLIC_URL}>
+    //   <Route exact path = '/' component = { Home } />
+    // </HashRouter>
   );
 }
 

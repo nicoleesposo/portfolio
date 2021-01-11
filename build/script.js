0f693c01c426678481dee597e559a0895ac84fe4
unknown <nicoleesposo.web@gmail.com>
2021-01-11 14:32:54 +0800

    Added express

---
+++ build/script.js
@@ -0,0 +1,31 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     var menu = document.getElementById('megamenu');
+//     var burger = document.getElementById('burger');
+//     burger.addEventListener('click', () => {
+//         menu.classList.toggle('hidden');
+//         console.log("working");
+//     });
+// });
+
+
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+
+//   var check = false;  
+
+//     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//         document.getElementById("full-links").style.display = "none";
+//         document.getElementById("burger").style.display = "block";
+//     } else {
+//         document.getElementById("full-links").style.display = "block";
+//         document.getElementById("burger").style.display = "none";
+//     }    
+// }    
+
+
+
+new kursor ({
+    type: 2,
+    removeDefaultCursor: true
+})
